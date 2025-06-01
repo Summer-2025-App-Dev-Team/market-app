a2f8fc6b928923a6c894efc3e272241528698720
YianXie <yian.xie@outlook.com>
2025-06-01 11:26:43 +0800

    testing remote branches

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,6 @@ import { createRoot } from "react-dom/client";
 createRoot(document.getElementById("root")).render(
     <>
         <h1>I'm the the best</h1>
-        <h2>Ian testing branches</h2>
+        <h2>Ian testing branches again!</h2>
     </>
 );
