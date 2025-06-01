cad76c3d759d6c02f8867f00a9be8a9c656f3ffa
YianXie <yian.xie@outlook.com>
2025-06-01 10:31:28 +0800

    testing commiting to branches

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")).render(<h1>I'm the the best</h1>);
+createRoot(document.getElementById("root")).render(
+    <>
+        <h1>I'm the the best</h1>
+        <h2>Ian testing branches</h2>
+    </>
+);
