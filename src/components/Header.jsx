8a6e38200f1afd223a7012c88708dc687b7960db
YianXie <yian.xie@outlook.com>
2025-06-04 20:32:27 +0800

    formatted code and rename main.jsx to index.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function() {
+    return (
+        <header>
+            <nav>
+                <span>Button 1</span>
+                <span>Button 2</span>
+                <span>Button 3</span>
+            </nav>
+        </header>
+    )
+}
--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-
-createRoot(document.getElementById("root")).render(
-    <>
-        <h1>I'm the the best</h1>
-        <h2>Ian testing branches again!</h2>
-    </>
-);
