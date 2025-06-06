4eede8702e2a1d358622778ecff5a382a332d41b
caleb craig <calebbhatta@icloud.com>
2025-06-06 09:18:15 +0800

    downloaded firebase and zustand, added firebase.js to initialise firebase

---
+++ src/components/lib/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCga2ChVR0jZ6fLoeXIPCN8ozyt2XO1IIw",
+  authDomain: "sas-marketplace.firebaseapp.com",
+  databaseURL: "https://sas-marketplace-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sas-marketplace",
+  storageBucket: "sas-marketplace.firebasestorage.app",
+  messagingSenderId: "332132025771",
+  appId: "1:332132025771:web:1d299c30236543ff4bfeee",
+  measurementId: "G-H5WCVF94VS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
