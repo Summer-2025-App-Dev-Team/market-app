0eeba78d411bdad3fed2cf09b17787a7ebe13ee3
Aryan Mahajan <mahajan802464@sas.edu.sg>
2025-06-09 22:03:24 +0800

    fix sas logo

--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -1,12 +1,12 @@
 import "../../assets/css/Header.css";
-import "../../assets/images/sas-logo.png"
+import logo from "../../assets/images/sas-logo.png"
 import LogoutOrLogin from "./LogoutOrLogin";
 
 export default function Header() {
     return (
         <header>
             <nav>
-                <img src="/src/assets/images/sas-logo.png" alt="SAS logo" draggable={false} onClick={function () { window.location.href = "/" }} />
+                <img src={logo} alt="SAS logo" draggable={false} onClick={function () { window.location.href = "/" }} />
                 <a href="#">Home</a>
                 <a href="#">About us</a>
                 <a href="#">Contact</a>
