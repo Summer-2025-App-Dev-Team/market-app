31cf0b1cc566b416892bbc8ca95b436115772b91
Aryan Mahajan <mahajan802464@sas.edu.sg>
2025-06-12 11:08:35 +0800

    lowercase css names

--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -1,15 +1,11 @@
 import "../../assets/css/header.css";
 import logo from "../../assets/images/sas-logo.png"
-import burger_button from "../../assets/svgs/burger-button.svg";
+import burger_button from "../../assets/icons/burger-button.svg";
 import LogoutOrLogin from "./LogoutOrLogin";
-import MobileHeader from "./MobileHeader";
-import { Link } from "react-router-dom";
 
 export default function Header() {
     function showBurger() {
         document.querySelector(".burger").classList.add("show");
-
-        // TODO: not a really good solution tbh, the setTimeOut can be improved
         setTimeout(() => {
             document.addEventListener("click", function handelOnClick(e) {
                 if (e.target.classList.contains("burger")) return;
@@ -23,14 +19,22 @@ export default function Header() {
         <header>
             <nav>
                 <img src={logo} alt="SAS logo" draggable={false} className="sas-logo" onClick={function () { window.location.href = "/" }} />
-                <Link to={"/"}>Home</Link>
-                <Link to={"/about"}>About us</Link>
-                <Link to={"/contact"}>Contact</Link>
+                <a href="#">Home</a>
+                <a href="#">About us</a>
+                <a href="#">Contact</a>
                 <LogoutOrLogin />
                 <img src={burger_button} alt="menu" draggable={false} className="show-mobile burger-button" onClick={showBurger} />
 
-                {/* For mobile or small screen devices */}
-                <MobileHeader />
+                <div className="show-mobile burger">
+                    <b>SAS Market App</b>
+                    <hr />
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><LogoutOrLogin /></li>
+                    </ul>
+                </div>
             </nav>
         </header>
     )
