caa65fee5d15a9bd4e25508078aa29fb9f5b5a18
Aryan Mahajan <mahajan802464@sas.edu.sg>
2025-06-18 14:37:50 +0530

    fix bug in FilterBar

--- src/components/item-page/FilterBar.jsx
+++ src/components/item-page/FilterBar.jsx
@@ -4,7 +4,7 @@ import styles from "../../assets/css/itempage.module.css";
 export default function FilterBar() {
     return (
         <div className={styles["scroll-wrapper"]}>
-            <div className={styles["scroll-container" style={{ "--show-before-shadow": 0 }}]}>
+            <div className={styles["scroll-container"]} style={{ "--show-before-shadow": 0 }}>
                 <FilterButton name="Website Design" />
                 <FilterButton name="Website Design" />
                 <FilterButton name="Website Design" />
