3de109544dac4df2848b0ec539a6a3fc98d8e382
Aryan Mahajan <mahajan802464@sas.edu.sg>
2025-06-18 17:18:22 +0530

    fix-footer

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "../../assets/css/footer.module.css";
 
 export default function Footer() {
     return (
-        <footer>
+        <footer className={styles.footer}>
             &copy; 2025 | SAS Market App
         </footer>
     )
