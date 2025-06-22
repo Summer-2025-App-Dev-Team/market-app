a0354178ed19d72fddfa806cc9bd3faac4f3f5d7
YianXie <yian.xie@outlook.com>
2025-06-22 16:44:15 +0800

    improve header and a few global properties

--- src/components/global/UserDropdown.jsx
+++ src/components/global/UserDropdown.jsx
@@ -4,9 +4,31 @@ import useAuthStore from "../store/useAuthStore";
 import dropdownIcon from "../../assets/svgs/dropdown-icon.svg";
 import styles from "../../assets/css/header.module.css";
 import { Link } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
 
 export default function UserDropdown() {
+    const [dropdownActived, setDropdownActived] = useState(false);
+    const dropdown = useRef(null);
     const user = useAuthStore((state) => state.user);
+
+    useEffect(() => {
+        const handleOutsideClick = (e) => {
+            if (
+                dropdown.current &&
+                !dropdown.current.contains(e.target) &&
+                !e.target.closest(`.${styles["user-dropdown"]}`)
+            ) {
+                dropdown.current.classList.remove(styles.active);
+                setDropdownActived(false);
+            }
+        }
+
+        document.addEventListener("click", handleOutsideClick);
+
+        return () => {
+            document.removeEventListener("click", handleOutsideClick);
+        }
+    }, [])
 
     // Still loading
     if (user === undefined) {
@@ -18,14 +40,23 @@ export default function UserDropdown() {
         return <LogoutOrLogin />;
     }
 
+    function handleOnClick() {
+        if (dropdownActived) {
+            dropdown.current.classList.remove(styles.active);
+        } else {
+            dropdown.current.classList.add(styles.active);
+        }
+        setDropdownActived(!dropdownActived);
+    }
+
     // If the user is logged in
     return (
-        <div className={styles["user-dropdown"]}>
+        <div className={styles["user-dropdown"]} onClick={handleOnClick}>
             <div className={styles["user"]}>
                 <Username />
                 <img src={dropdownIcon} alt="dropdown" draggable={false} />
             </div>
-            <ul>
+            <ul ref={dropdown}>
                 <li><Link to={"#"}>View profile</Link></li>
                 <li><Link to={"#"}>Settings</Link></li>
                 <li><LogoutOrLogin /></li>
