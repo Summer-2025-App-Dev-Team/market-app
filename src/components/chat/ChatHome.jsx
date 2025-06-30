ebeb857a5263c105116ba0a5129823975a3fc26e
harry <harryjaewon@gmail.com>
2025-06-30 11:26:43 +0900

    tried to add chat feature

--- src/components/chat/ChatHome.jsx
+++ src/components/chat/ChatHome.jsx
@@ -1,31 +1,87 @@
 import { Outlet } from "react-router-dom";
 import ChatRoomPreview from "./ChatRoomPreview";
 import styles from "../../assets/css/chathome.module.css";
+import { ref, getDatabase, set } from "firebase/database";
+import { auth, db, realtimeDB } from "../lib/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
 
+useEffect(() => {
+    //runs when the component mounts and in reloads
+    function unsubscribe {
+    
+    
+        function getUser() {
+            onAuthStateChanged(
+                auth,
+                /*runs when page loads, user logs in, or user logs out. Used cuz currentUser doesn't wait until the website finishes getting user info*/
+                (user) => {
+                    /*passed current user object or null if empty. function that will be called whenever the user status changes*/
+                    const user = auth.currentUser;
+                    if (user) {
+                        console.log("current user", user);
+                    } else {
+                        console.log("No user is loged in");
+                    }
+
+                }
+            )
+    
+    
+    
+        }
+    }
+}, []);
+    
+
+
+
+const chatsRef = ref(realtimeDB, "chats/");
+set(chatsRef, {});
 export default function ChatHome() {
-    // This is the default chat home component, when there is no specific chat room selected.
+  // This is the default chat home component, when there is no specific chat room selected.
 
-    return (
-        <div className={styles.container}>
-            <ul className={styles.sidebar}>
-                <li>
-                    <h1>Your chats</h1>
-                </li>
-                <li><ChatRoomPreview /></li>
-                <li><ChatRoomPreview /></li>
-                <li><ChatRoomPreview /></li>
-                <li><ChatRoomPreview /></li>
-                <li><ChatRoomPreview /></li>
-                <li><ChatRoomPreview /></li>
-                <li><ChatRoomPreview /></li>
-                <li><ChatRoomPreview /></li>
-                <li><ChatRoomPreview /></li>
-                <li><ChatRoomPreview /></li>
-                <li>SAS Market App</li>
-            </ul>
-            <div>
-                <Outlet />
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <ul className={styles.sidebar}>
+        <li>
+          <h1>Your chats</h1>
+        </li>
+        <li>
+          <ChatRoomPreview />
+        </li>
+        <li>
+          <ChatRoomPreview />
+        </li>
+        <li>
+          <ChatRoomPreview />
+        </li>
+        <li>
+          <ChatRoomPreview />
+        </li>
+        <li>
+          <ChatRoomPreview />
+        </li>
+        <li>
+          <ChatRoomPreview />
+        </li>
+        <li>
+          <ChatRoomPreview />
+        </li>
+        <li>
+          <ChatRoomPreview />
+        </li>
+        <li>
+          <ChatRoomPreview />
+        </li>
+        <li>
+          <ChatRoomPreview />
+        </li>
+        <li>SAS Market App</li>
+      </ul>
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
 }
