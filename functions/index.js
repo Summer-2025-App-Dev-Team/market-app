13e01679e4f9a058ba663112d49b2ca2038b4609
YianXie <yian.xie@outlook.com>
2025-07-08 21:35:40 +0800

    added email functionality

--- functions/index.js
+++ functions/index.js
@@ -16,4 +16,16 @@ export const scheduledDeleteExpiredDocs = onSchedule("every day 08:00", async ()
     const deletePromises = snapshot.docs.map((doc) => doc.ref.delete());
     console.log(`Deleted ${deletePromises.length} expired documents.`);
     await Promise.all(deletePromises);
+
+    // For email
+    const emailDoc = {
+        to: "yianxie52@gmail.com", // TODO: this should be changed to the seller's email later
+        message: {
+            subject: "Some item has expired!",
+            text: "This is the plain text part of the email",
+            html: `<p>Successfully deleted <b>${deletePromises.length}</b> expired documents</p>`
+        }
+    }
+
+    await db.collection("mail").add(emailDoc);
 });
